ff28ce26363ee4f555054660195fb2c90f67819c
LP-Forever <455958742@qq.com>
2026-03-11 09:56:43 +0000

    fix

--- gcr_lerobot_1/lerobot/common/policies/pi05/modeling_pi05.py
+++ gcr_lerobot_1/lerobot/common/policies/pi05/modeling_pi05.py
@@ -212,7 +212,7 @@ class PI05Policy(PreTrainedPolicy):
 
         # tokenizer_path = "/Data/lzl/huggingface/paligemma-3b-pt-224"
         # tokenizer_path = "/mnt/wangxiaofa/RDT_module_params/paligemma-3b-pt-224/"
-        # huggingface文件，按需改动
+        # 按需改动
         tokenizer_path = "/mnt/wangxiaofa/RDT_module_params/paligemma-3b-pt-224"
         self.language_tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
